aa086941c420e8d2b52437991752972853f41341
popura <gzyoung2330351551@gmail.com>
2026-05-20 16:48:47 +0800

    2

--- quantgpt/validation/__init__.py
+++ quantgpt/validation/__init__.py
@@ -2,11 +2,14 @@
 
 from .oos_backtest import run_factor_oos_backtest, to_public_oos_result
 from .oos_score import compute_oos_score
+from .promotion import build_factor_validation_provenance, evaluate_promotion_provenance
 from .split import OOSConfig, split_by_dates
 
 __all__ = [
     "OOSConfig",
+    "build_factor_validation_provenance",
     "compute_oos_score",
+    "evaluate_promotion_provenance",
     "run_factor_oos_backtest",
     "split_by_dates",
     "to_public_oos_result",
